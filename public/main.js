067910d5ede5d0a26380ce63784046c417df2865
Mara Caoile <mara.caoile@gmail.com>
2023-05-21 19:33:59 -0500

    loop is working

--- public/main.js
+++ public/main.js
@@ -2,16 +2,67 @@
 // import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
-import { renderJokeSetup } from './renderJokes';
+import renderToDom from './renderToDOM';
+import { getJokeSetup, getJokeDelivery } from '../api/promises';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>Mara's Joke Generator</h1>
-    <button class="btn btn-danger" id="getJoke">Get Joke</button>`;
+const title = () => {
+  const domString = `<div class="generator-title"><h1>Programming Joke Generator</h1>
+  </div>`;
+  renderToDom('#generatorTitle', domString);
+};
+title();
 
-  document
-    .querySelector('#getJoke')
-    .addEventListener('click', () => renderJokeSetup());
+const jokeBtn = () => {
+  const btnString = `<button class="btn btn-danger" 
+  id="getNewJoke">New Joke</button>`;
+  renderToDom('#btnContainer', btnString);
 };
 
+const punchLineBtn = () => {
+  const btnString = `<button class="btn btn-danger" 
+  id="getPunchline">Get Punchline</button>`;
+  renderToDom('#btnContainer', btnString);
+};
+
+const renderJokeSetup = () => {
+  getJokeSetup().then((response) => {
+    const domString = `<h2>${response}</h2>`;
+    punchLineBtn();
+    renderToDom('#app', domString);
+  });
+};
+
+const renderJokeDelivery = () => {
+  getJokeDelivery().then((response) => {
+    const domString = `<h2>${response}</h2>`;
+    jokeBtn();
+    renderToDom('#app', domString);
+  });
+};
+
+const init = () => {
+  const jokeStart = () => {
+    const domString = `<button class="btn btn-danger" id="getJoke">Get Joke</button>
+    </div>`;
+    renderToDom('#btnContainer', domString);
+  };
+  jokeStart();
+};
 init();
+
+document.querySelector('#btnContainer').addEventListener('click', (e) => {
+  e.preventDefault();
+  switch (e.target.id) {
+    case 'getJoke':
+      renderJokeSetup();
+      break;
+    case 'getPunchline':
+      renderJokeDelivery();
+      break;
+    case 'getNewJoke':
+      renderJokeSetup();
+      break;
+    default:
+      break;
+  }
+});
